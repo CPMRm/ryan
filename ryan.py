3e32a0eeb22d8768e5081f039d4135fd1edcca6c
CPMRm <rmcpmstore@gmail.com>
2025-05-05 11:36:37 +0800

    Update ryan.py

--- ryan.py
+++ ryan.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 __ENDPOINT_URL__: str = "https://ryancpm.squareweb.app/api"
 
-class ryan:
+class Ryan:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
